23764140b81e4a9f405d044a7a2e6b526d3b2b4a
Corey Moore <monorthwest@gmail.com>
2019-06-13 13:02:12 -0700

    added stars to basic html mock up

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -1,1 +1,25 @@
 'use strict';
+
+
+const bookmarkApp = (function(){
+
+  //when add bookmark button is clicked, hide button and display add bookmark form field
+  function displayAddBookmarks() {
+    //     $('.js-add-bookmark').click(function() {$('.js-bookmarks-form').show(
+    //   //html for form
+    //     `
+    //     <li><label for="bookmarks-entry">Add Bookmark</label></li>
+    //     <li><input id="bookmarks-entry" type="text" name="bookmarks-entry" class="js-bookmarks-entry" placeholder="Cats"></li>
+    //     <li></li>
+    //     `
+    //   )} 
+  }   
+ 
+  //function handleNewBookmarkSubmit(){}
+
+  return{
+    displayAddBookmarks
+
+  };
+
+})();
--- scripts/store.js
+++ scripts/store.js
@@ -1,1 +1,18 @@
 'use strict';
+// example STORE array
+// [
+//     {
+//         "id": "cjwv2x3h8000y0kygwo1avwrj",
+//         "title": "bookmark",
+//         "url": "https://www.fdsf.com/cats",
+//         "desc": null,
+//         "rating": null
+//     },
+//     {
+//         "id": "cjwv2yq8c000z0kygjcsjv4ut",
+//         "title": "bookmark2",
+//         "url": "https://www.catmania.com/cats",
+//         "desc": "this is a place for cats",
+//         "rating": 4
+//     }
+// ]
