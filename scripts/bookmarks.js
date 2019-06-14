ca2bcca5251e43b22c621f2b5ac1408cb5c953c0
Corey Moore <monorthwest@gmail.com>
2019-06-13 19:00:37 -0700

    added css and edited html form popup

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -7,29 +7,42 @@
 function handleNewBookmarkClicked() {
   $('#js-add-bookmark').click(function() {$('.form-container').html(
     `
-            <form id="js-addForm" class="formDisplay">
-            <div>
-                <label for="bookmarks-entry">Title</label>
-                <input name="title" id="new-title-bookmark" type="text" class="js-new-bookmark" placeholder="Bookmark title" required>
-            </div>
-            <div>
-                <label for="bookmarks-entry">URL</label>
-                <input name="url" id="new-url-bookmark" type="link" class="js-new-bookmark" placeholder="Your link to site here" required>
-            </div>
-            <div>
-                <label for="new-bookmark">Description</label>
-                <input name="description" id="new-dscrp-bookmark" type="text" class="js-new-bookmark" placeholder="What is at your bookmark" required>
-            </div>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-            
-            <button id="submitForm" type="submit">Submit</button>
+    <form id="js-addForm" class="formDisplay">
+    <div>
+      <label for="bookmarks-entry">Title</label>
+      <input name="title" id="new-title-bookmark" type="text" class="js-new-bookmark" placeholder="Bookmark title" required>
+    </div>
+    <div>
+      <label for="bookmarks-entry">URL</label>
+      <input name="url" id="new-url-bookmark" type="link" class="js-new-bookmark" placeholder="URL here" required>
+    </div>
+    <div>
+      <label for="new-bookmark">Description</label>
+      <input name="description" id="new-dscrp-bookmark" type="text" class="js-new-bookmark" placeholder="Describe your bookmark" required>
+    </div>
+    <div class="radio-buttons" >
+    <label for="title" class="radio-title">Give it a rating!</label>
       
-        </form>
-            `
+    <label for="one" class="label">1</label>
+      <input type="radio" name="rating" value="1" class="radio-button">
+      
+    <label for="two" class="label">2</label>
+      <input type="radio" name="rating" value="2" class="radio-button">
+      
+    <label for="three" class="label">3</label>
+      <input type="radio" name="rating" value="3" class="radio-button">
+      
+    <label for="four" class="label">4</label>
+      <input type="radio" name="rating" value="4" class="radio-button">
+      
+    <label for="five" class="label">5</label>
+      <input type="radio" name="rating" value="5" class="radio-button">
+    </div>
+            
+    <button id="submitForm" type="submit">Submit</button>
+      
+    </form>
+   `
   );
   });
 }
